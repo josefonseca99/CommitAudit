ad85f4a5e46c6b0fbb3fadfbabc32cfad8f686dc
Ana Rendon <arendon@qvision.us>
2021-03-11 09:52:26 -0500

    Base de datos QVision

--- config_files/DatabaseConfiguration.py
+++ config_files/DatabaseConfiguration.py
@@ -1,6 +1,6 @@
 DATABASE = {
-    'HOST_DIRECTION': 'localhost',
+    'HOST_DIRECTION': '192.168.100.28',
     'DB_USERNAME': 'root',
-    'PASSWORD': 'qvmysql',
+    'PASSWORD': 'CaGcQuality@1.43',
     'DATABASE': 'rendimientobbia',
 }
