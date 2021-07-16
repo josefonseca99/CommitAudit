b72cc2008ba7a71f31e82b90883c8ad3ed8837db
Administrator <jfonseca@qvision.us>
2021-07-16 11:48:49 -0500

    Update para sprint 116

--- config_files/APIConfiguration.py
+++ config_files/APIConfiguration.py
@@ -1,4 +1,4 @@
 AZURE_DEV_LINK = "dev.azure.com"
 ORGANIZATION = "grupobancolombia"
 PROJECT = "b267af7c-3233-4ad1-97b3-91083943100d"
-BANCOLOMBIA_API_TOKEN = ":loohdxzsaufxbkbjdhalc3ksmrbvey5vlp7zg27rvhaef2n34lxq"
+BANCOLOMBIA_API_TOKEN = ":rfjflvmz3kqqyv4kb6f3qvfuy2pmgyld6eiwdmneleetkmceuhhq"
