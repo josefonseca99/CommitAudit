3117abe0f786a0a527c76ac8216e99af3a91dc3e
Administrator <jfonseca@qvision.us>
2021-07-19 14:29:03 -0500

    Nuevo token

--- config_files/APIConfiguration.py
+++ config_files/APIConfiguration.py
@@ -1,4 +1,4 @@
 AZURE_DEV_LINK = "dev.azure.com"
 ORGANIZATION = "grupobancolombia"
 PROJECT = "b267af7c-3233-4ad1-97b3-91083943100d"
-BANCOLOMBIA_API_TOKEN = ":rfjflvmz3kqqyv4kb6f3qvfuy2pmgyld6eiwdmneleetkmceuhhq"
+BANCOLOMBIA_API_TOKEN = ":u4qpjxgqlokmk77s42lu2uht4f7lbuf2miipbty4xt6ifeh2cqga"
