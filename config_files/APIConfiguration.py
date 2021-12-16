d20c6ffa5a540f92db6df696bcb791f2d999af8d
Administrator <jfonseca@qvision.us>
2021-12-16 11:34:39 -0500

    Actualización del token para el consumo de los servicios

--- config_files/APIConfiguration.py
+++ config_files/APIConfiguration.py
@@ -1,4 +1,4 @@
 AZURE_DEV_LINK = "dev.azure.com"
 ORGANIZATION = "grupobancolombia"
 PROJECT = "b267af7c-3233-4ad1-97b3-91083943100d"
-BANCOLOMBIA_API_TOKEN = ":u4qpjxgqlokmk77s42lu2uht4f7lbuf2miipbty4xt6ifeh2cqga"
+BANCOLOMBIA_API_TOKEN = ":j5n7s23fehyhrfifz6c6fdnzsrnat4swc2ampkkvo24vz2otehga"
